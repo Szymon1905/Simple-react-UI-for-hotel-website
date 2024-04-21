2db85af157b7dbd5b1660134d3667b1f2e93bd26
Szymon Borzdyński <szborzdynski@gmail.com>
2024-04-21 22:36:04 +0200

    fix do przekreślania

--- lab2/script.js
+++ lab2/script.js
@@ -3,7 +3,7 @@ var elements_from_lists = note_list.getElementsByTagName("li");
 for (let i = 0; i < elements_from_lists.length; i++) {
     var delete_button = document.createElement("span");
     //var delete_button_text = document.createTextNode("\u00D7");
-    delete_button.className = "close";
+    delete_button.className = "delete_button";
     //delete_button.appendChild(delete_button_text);
 
     elements_from_lists[i].appendChild(delete_button);
@@ -26,6 +26,40 @@ function add_new_note() {
     var note_list = document.getElementById("note_list");
     var elements_from_list = note_list.getElementsByTagName("li");
     var new_element = elements_from_list[elements_from_list.length - 1];
+
+
+    var delete_button = make_delete_button();
+    
+    new_element.appendChild(delete_button);
+}
+
+function usun_element() {
+    var element = this.parentElement;
+    element.style.display = "none";
+}
+
+
+
+function mark_as_done() {
+    if (this.style.textDecoration === ""){
+        this.style.textDecoration = "line-through";
+        this.style.backgroundColor = "grey";
+        var today = new Date().toDateString();
+        this.textContent += " -" + today;
+        this.append(make_delete_button());
+    } else{
+        this.style.textDecoration = "";
+        this.style.backgroundColor = "white";
+        
+        var index = this.textContent.indexOf(" -");
+        if (index !== -1) {
+            this.textContent = this.textContent.substring(0, index);
+        }
+        this.append(make_delete_button());
+    }
+}
+
+function make_delete_button(){
     var delete_button = document.createElement("span");
 
 
@@ -38,36 +72,9 @@ function add_new_note() {
     delete_button.style.width = "42px";
     delete_button.style.height = "45px";
 
-    delete_button.className = "close";
+    delete_button.className = "delete_button";
     delete_button.onclick = usun_element;
-
     delete_button.appendChild(div_for_button);
-    new_element.appendChild(delete_button);
+
+    return delete_button;
 }
-
-function usun_element() {
-    var element = this.parentElement;
-    element.style.display = "none";
-}
-
-var note_list = document.getElementById("note_list");
-var elements_from_lists = note_list.getElementsByTagName("li");
-elements_from_lists[0]
-
-function mark_as_done() {
-    if (this.style.textDecoration === ""){
-        this.style.textDecoration = "line-through";
-        this.style.backgroundColor = "grey";
-        var today = new Date().toDateString();
-        this.textContent += " -" + today;
-    } else{
-        this.style.textDecoration = "";
-        this.style.backgroundColor = "white";
-
-        var index = this.textContent.indexOf(" -");
-        if (index !== -1) {
-            this.textContent = this.textContent.substring(0, index);
-        }
-    }
-}
-
